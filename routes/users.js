b5438c5c1bd97fa2634ed03436944088b85120ef
SUHWAN KIM <a75345005@gmail.com>
2024-07-15 17:45:10 +0900

    kakao

--- routes/users.js
+++ routes/users.js
@@ -77,6 +77,40 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.post('/kakaologin', async (req, res) => {
+  const { u_id, u_nickname } = req.body;
+  const conn = await getConn();
+  const selectQuery = 'SELECT u_id, u_password, u_nickname FROM Users WHERE u_id = ?';
+  const insertQuery = 'INSERT INTO Users (u_id, u_password, u_nickname) VALUES (?, ?, ?)';
+
+  try {
+    // uid로 사용자 정보 조회
+    let [rows, fields] = await conn.query(selectQuery, [u_id]);
+    
+    if (rows.length > 0) {
+      const user = rows[0];
+      // 비밀번호 확인
+      if (user.u_password === u_password) {
+        // 로그인 성공 시 유저 정보 반환
+        const { u_id, u_nickname } = user;
+        conn.release();
+        res.status(200).json({ u_id, u_nickname });
+      } else {
+        // 비밀번호가 일치하지 않는 경우
+        conn.release();
+        res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+      }
+    } else {
+      // uid가 존재하지 않는 경우, 새로운 유저 생성
+      await conn.query(insertQuery, [u_id, u_id, u_nickname]);
+      conn.release();
+      res.status(201).json({ u_id, u_nickname });
+    }
+  } catch (error) {
+    conn.release();
+    res.status(500).json({ message: '로그인 중 오류가 발생했습니다.', error });
+  }
+});
 
 
 // // 유저 정보 불러오기
