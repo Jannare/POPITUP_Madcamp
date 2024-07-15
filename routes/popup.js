8a3f187e41c727954cc52bb2a8e7286aaf494350
SUHWAN KIM <a75345005@gmail.com>
2024-07-15 23:45:13 +0900

    getall

--- routes/popup.js
+++ routes/popup.js
@@ -15,12 +15,35 @@ const getConn = async() => {
   return await pool.getConnection(async (conn) => conn);
 };
 
+router.get('/getAll', async (req, res) => {
+  const conn = await getConn();
+
+  const selectAllQuery = 'SELECT * FROM popupstore';
+
+  try {
+    const [rows, fields] = await conn.query(selectAllQuery);
+    conn.release();
+
+    if (rows.length > 0) {
+
+      res.status(200).send(rows); // 전체 데이터 전송
+    } else {
+      res.status(404).send({ message: 'No records found' });
+    }
+  } catch (error) {
+    conn.release();
+    res.status(500).send({ message: 'Error fetching data', error });
+  }
+});
+
+
+
 // 특정 p_id에 해당하는 데이터를 가져오는 라우터
 router.get('/get/:p_id', async (req, res) => {
   const p_id = req.params.p_id; // URL 파라미터에서 pid 값을 가져옴
   const conn = await getConn();
 
-  const selectQuery = 'SELECT p_id, p_name, p_location, p_region, p_latitude, p_longitude, p_startdate, p_enddate, p_status, p_intro, p_detail, p_interest, p_imageurl FROM popupstore WHERE p_id = ?';
+  const selectQuery = 'SELECT p_id, p_name, p_location, p_simplelocation, p_region, p_latitude, p_longitude, p_startdate, p_enddate, p_status, p_intro, p_detail, p_interest, p_imageurl FROM popupstore WHERE p_id = ?';
   const filename = `${p_id}image.png`;
   const filepath = `http://3.34.195.223:3000/images/${filename}`; // 로컬 파일 경로를 URL 경로로 변환
 
@@ -34,8 +57,6 @@ router.get('/get/:p_id', async (req, res) => {
   END
   WHERE p_id = ?;`;
 
-
-
   try {
     // p_imageurl 필드를 업데이트
     await conn.query(updateQuery, [filepath, p_id]);
@@ -46,19 +67,6 @@ router.get('/get/:p_id', async (req, res) => {
     conn.release();
 
     if (rows.length > 0) {
-      // 날짜 필드를 년/월/일 형식으로 변환
-      const row = rows[0];
-      const formatDate = (date) => {
-        const d = new Date(date);
-        const year = d.getFullYear();
-        const month = ('0' + (d.getMonth() + 1)).slice(-2);
-        const day = ('0' + d.getDate()).slice(-2);
-        return `${year}/${month}/${day}`;
-      };
-      
-      
-      // row.p_startdate = formatDate(p_startdate);
-      // row.p_enddate = formatDate(p_enddate);
       conn.release();
       res.status(200).send(rows[0]); // 결과 행을 포함해 데이터 전송
     } else {
