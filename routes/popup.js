0315e697767f67a6b9280315c5a34995c031ad43
SUHWAN KIM <a75345005@gmail.com>
2024-07-17 01:12:20 +0900

    1

--- routes/popup.js
+++ routes/popup.js
@@ -211,10 +211,10 @@ router.post('/toggleFavorite', async (req, res) => {
 });
 
 router.post('/myreview', async (req, res) => {
-  const { u_id } = req.body;
+  const conn = await getConn();
+  const {u_id} = req.body;
   console.log('u_id:', u_id); // u_id 값을 터미널에 출력
   
-  const conn = await getConn();
 
   const selectQuery = `
     SELECT p_id FROM popupstore_users_reviews WHERE u_id = ?
