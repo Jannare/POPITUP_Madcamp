ba2d374d462a8fdcfdf0ac474688d2b91c9d6afc
SUHWAN KIM <a75345005@gmail.com>
2024-07-16 20:05:57 +0900

    1

--- routes/popup.js
+++ routes/popup.js
@@ -227,6 +227,8 @@ router.post('/toggleFavorite', async (req, res) => {
 
 router.post('/review', async (req, res) => {
   const { p_id } = req.body;
+  console.log('p_id:', p_id); // p_id 값을 터미널에 출력
+
   const conn = await getConn();
 
   const selectQuery = `
@@ -249,8 +251,35 @@ router.post('/review', async (req, res) => {
   }
 });
 
+router.post('/review/post', async (req, res) => {
+  const { u_id, p_id, grade, review  } = req.body;
+  const conn = await getConn();
 
+  const selectQuery = `
+    SELECT u_nickname, grade, review FROM popupstore_users_reviews WHERE u_id = AND p_id = ?
+  `;
 
+  const insertQuery = 'INSERT INTO popupstore_users_reviews (u_id, p_id, grade, review) VALUES (?, ?, ?, ?)';
+
+  try {
+    const [rows] = await conn.query(selectQuery, [u_id, p_id]);
+    
+    if (rows.length === 0) {
+      await conn.query(insertQuery, [u_id, p_id, grade, review]);
+      res.status(201).json({message: 'Review added successfully'})
+      const [rows] = await conn.query(selectQuery, [u_id, p_id]);
+      res.status(200).json(rows);
+    } else{
+      res.status(409).json({message: 'Review already exists'});
+    }
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    conn.release();
+  }
+});
 
 
 module.exports = router;
